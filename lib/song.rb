33ea91871a08dfa79921fc8cccdd381d2e4f9c74
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-04 05:02:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 class Song
-  @@count = 0
   attr_accessor :name, :artist, :genre
+  @@count = 0
   def initialize(name, artist, genre)
+    @@count
     @name = name
     @artist = artist
     @genre = genre
